5d21020132bdee0d0a874f6335a2c4ff0a5eee2e
Alex <ac00gye@gmail.com>
2022-02-24 02:56:40 -0300

    Local storage task

--- script.js
+++ script.js
@@ -1,58 +1,55 @@
-let clickButton = document.getElementById("btnName")
-clickButton.onclick = () => {
-   let name = document.getElementById("name")
-   name = name.value
-   if (name === "") {
-    let warning = document.getElementById("warning")
-    warning.className = "text-center text-danger mt-5"
-   } else {
-    let hello = document.getElementById("hi")
-    hello.className = "text-primary"
-    hello.innerHTML = name 
-    document.getElementById("first-container").className = "d-none"
-    warning.className = "d-none"
-    document.getElementById("second-container").className = "row content align-items-center justify-content-center"
-   }
-}
+// let clickButton = document.getElementById("btnName")
+// clickButton.onclick = () => {
+//    let name = document.getElementById("name")
+//    name = name.value
+//    if (name === "") {
+//     let warning = document.getElementById("warning")
+//     warning.className = "text-center text-danger mt-5"
+//    } else {
+//     let hello = document.getElementById("hi")
+//     hello.className = "text-primary"
+//     hello.innerHTML = name 
+//     document.getElementById("first-container").className = "d-none"
+//     warning.className = "d-none"
+//     document.getElementById("second-container").className = "row content align-items-center justify-content-center"
+//    }
+// }
 
 
 let trigger = document.getElementById("btnList")
 let list = []
 let counter = 0
-
+let quantity = localStorage.getItem("task")
 trigger.onclick = () =>  {
-    c = counter++
-    let text = document.getElementById("text")
-    text = text.value
-    list.push(text)
-    localStorage.setItem("counter", c)
-    for (i=0;i<list.length;i++) {
-        localStorage.setItem("task n " + i, list[i])
+    if (quantity === null) { 
+        let text = document.getElementById("text")
+        text = text.value
+        list.push(text)
+        localStorage.setItem("task", JSON.stringify(list))
+        let bullet = document.createElement("li")
+        let task = document.createTextNode(text)
+        bullet.appendChild(task)
+        let currentDiv = document.getElementById("items")
+        let newBullet = document.body.insertBefore(bullet, currentDiv);
+        currentDiv.appendChild(newBullet)        
+    } else {
+        let text = document.getElementById("text")
+        let stored = JSON.parse(localStorage.getItem("task"))
+        stored.push(text.value)
+        localStorage.setItem("task", JSON.stringify(stored))
+        list = JSON.parse(localStorage.getItem("task"))
+        console.log(list)
     }
+
     }
-    quantity = localStorage.getItem("counter")
-    console.log(quantity)
-    // localStorage.setItem("task n " + quantity, list[quantity])
-
-
+    let chores = JSON.parse(localStorage.getItem("task"))
     
-        
+        for (i=0;i<chores.length;i++) {
+            let bullet = document.createElement("li")
+            let task = document.createTextNode(chores[i])
+            bullet.appendChild(task)
+            let currentDiv = document.getElementById("items")
+            let newBullet = document.body.insertBefore(bullet, currentDiv);
+            currentDiv.appendChild(newBullet)
 
-    // let bullet = document.createElement("li")
-    // let task = document.createTextNode(text)
-    // bullet.appendChild(task)
-    // let currentDiv = document.getElementById("list")
-    // let newBullet = document.body.insertBefore(bullet, currentDiv);
-    // currentDiv.appendChild(newBullet)
-
-
-
-
-
-
-
-
-
-
-
-
+    }
